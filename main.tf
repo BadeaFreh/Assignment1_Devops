4331a0bdc377de2c5c6e3abc1e639853790f7de5
Badea Freh <badeafreh@gmail.com>
2022-04-10 21:39:36 +0300

    spelling corrections

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_vpc" "user7" {
     # meaning: 3 octets for network (24 ones)
     #          1 octet for the hosts
-    # 192.168.7.0 is an example of one of these subnets
+    # 10.120.7.0 is an example of one of these subnets
     # (it can contain ~(2^8) hosts in it)
     # we only need 20 ipv4 ip addresses in this assignment, this is more than enough
   cidr_block = "10.120.7.0/24"
@@ -27,15 +27,14 @@ resource "aws_vpc" "user7" {
 resource "aws_subnet" "web_subnet" {
     vpc_id = aws_vpc.user7.id
   # (27 bits for the subnet, 5 bits for hosts)
-  # in ordr to mak 20 availabl ip addrsss, w nd 2^5 bits
+  # in order to make 20 available ip addresses, we need 2^5 bits
   cidr_block = "10.120.7.0/27"
-
 }
 
 # App Subnet
 resource "aws_subnet" "app_subnet" {
-  # why 32 bits on th last octt? first 32 ips (0-31) ar for wb subnt
-  # th first mpty ip is x.x.x.32
+  # why 32 bits on the last octet? first 32 ips (0-31) ar for web subnet
+  # the first empty ip is x.x.x.32
   vpc_id = aws_vpc.user7.id
   cidr_block = "10.120.7.32/27"
 }
@@ -43,6 +42,6 @@ resource "aws_subnet" "app_subnet" {
 # DB Subnet
 resource "aws_subnet" "db_subnet" {
   vpc_id = aws_vpc.user7.id
-  # 28 bits for th ntwork, 4 bits for th hosts
+  # 28 bits for the network, 4 bits for the hosts
   cidr_block = "10.120.7.64/28"
 }
